e1240812d912dd2919e144ad10d632f206afde8a
utensil <utensilcandel@gmail.com>
2016-07-10 16:35:26 +0700

    Properly handle anchor links

--- src/plugins/gimmick.linkfixer.js
+++ src/plugins/gimmick.linkfixer.js
@@ -1,6 +1,28 @@
 $.md.stage('pregimmick').subscribe((done) => {
+  let getCurrentMdUrlBase = () => {
+    let hash = window.location.hash.toString()
+    let parts = hash.split('#!')
+    if (parts.length >= 2) {
+      return parts[parts.length - 1].replace(/[^/]+\.md(#.+)?/, '')
+    }
+  }
+
   let baseUrl = window.$.md.config.baseUrl
-  let currentMdUrlBase = window.location.hash.replace(/[^/]+\.md/, '')
+  let currentMdUrlBase = getCurrentMdUrlBase()
+
+  let getCurrentMdRelativeUrl = () => {
+    let hash = window.location.hash.toString()
+    let parts = hash.split('#!')
+    if (parts.length >= 2) {
+      let mdFileAndAnchor = parts[parts.length - 1].split('#')
+      if (mdFileAndAnchor.length === 1 || mdFileAndAnchor.length === 2) {
+        return mdFileAndAnchor[0]
+      }
+    }
+
+    // TODO all unhandled exceptional case goes to index.md for now
+    return 'index.md'
+  }
 
   let processUrl = (url) => {
     let isExternalLink = /:\/\//.test(url)
@@ -13,18 +35,19 @@ $.md.stage('pregimmick').subscribe((done) => {
 
     if (isExternalLink) {
       ret.target = '_blank'
-    } else if (!isAnchor) {
-      if (isMdLink) {
-        ret.url = (currentMdUrlBase + url).replace(/\/\//g, '/')
-      } else {
-        ret.url = (baseUrl + '/' + currentMdUrlBase.replace('#!', '') + url).replace(/\/\//g, '/')
+    } else {
+      if (!isAnchor) {
+        if (isMdLink) {
+          ret.url = (currentMdUrlBase + url).replace(/\/\//g, '/')
+        } else {
+          ret.url = (baseUrl + '/' + currentMdUrlBase.replace('#!', '') + url).replace(/\/\//g, '/')
+        }
       }
     }
-
     return ret
   }
 
-  $('#md-content a').each(function (index, linkElement) {
+  $('#md-content a').each((index, linkElement) => {
     let url = $(linkElement).attr('href')
     // console.log('link:', url)
     let urlInfo = processUrl(url)
@@ -35,7 +58,18 @@ $.md.stage('pregimmick').subscribe((done) => {
     }
   })
 
-  $('#md-content img').each(function (index, linkElement) {
+  $('#md-content a.anchor').each((index, linkElement) => {
+    let url = $(linkElement).attr('href')
+    let parts = url.split('#')
+    let anchor = parts[parts.length - 1] || ''
+    // console.log('anchor:', anchor)
+    // console.log('getCurrentMdRelativeUrl() =>', getCurrentMdRelativeUrl())
+    let urlFixed = `#!${getCurrentMdRelativeUrl()}#${anchor}`.replace(/\/\//g, '/')
+
+    $(linkElement).attr('href', urlFixed)
+  })
+
+  $('#md-content img').each((index, linkElement) => {
     let url = $(linkElement).attr('src')
     // console.log('image:', url)
     let urlInfo = processUrl(url)
